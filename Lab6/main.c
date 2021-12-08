01cff24f62d11abb8ea7b0e16f0e77e82d1decf6
Luka Takeshi Kato <luka.kato@tuntscorp.com>
2021-12-08 19:02:51 -0300

    feat: screenshots

--- Lab6/main.c
+++ Lab6/main.c
@@ -15,6 +15,18 @@ TX_THREAD thread1;
 TX_THREAD thread2;
 TX_THREAD thread3;
 
+TX_THREAD thread4;
+TX_THREAD thread5;
+TX_THREAD thread6;
+
+TX_THREAD thread7;
+TX_THREAD thread8;
+TX_THREAD thread9;
+
+TX_THREAD thread10;
+TX_THREAD thread11;
+TX_THREAD thread12;
+
 TX_BYTE_POOL byte_pool;
 UINT status;
 
@@ -47,27 +59,76 @@ void tx_application_define(void *first_unused_memory) {
    
    status = tx_byte_pool_create(&byte_pool, "Block of memory", byte_pool_memory, BYTE_POOL_SIZE);
 
+   // A ---------------------------------------------------------------------------
+   
    status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE,TX_NO_WAIT);
 
    status = tx_thread_create(
       &thread1,
       "thread 1",
-      thread_1,
+      thread_fn,
       30,
       pointer,
       STACK_SIZE,
       1,
       1,
-      TX_NO_TIME_SLICE,
+      50,
       TX_AUTO_START
-   );
+  );
    
    status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE, TX_NO_WAIT);
    
    status = tx_thread_create(
       &thread2,
       "thread 2",
-      thread_2,
+      thread_fn,
+      30,
+      pointer,
+      STACK_SIZE,
+      1,
+      1,
+      50,
+      TX_AUTO_START
+    );
+   
+   status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE, TX_NO_WAIT);
+
+   status = tx_thread_create(
+      &thread3,
+      "thread 3",
+      thread_fn,
+      30,
+      pointer,
+      STACK_SIZE,
+      1,
+      1,
+      50,
+      TX_AUTO_START
+    );
+   
+   // B ---------------------------------------------------------------------------
+   
+   status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE,TX_NO_WAIT);
+
+   status = tx_thread_create(
+      &thread4,
+      "thread 4",
+      thread_fn,
+      30,
+      pointer,
+      STACK_SIZE,
+      1,
+      1,
+      TX_NO_TIME_SLICE,
+      TX_AUTO_START
+   );
+   
+   status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE, TX_NO_WAIT);
+   
+   status = tx_thread_create(
+      &thread5,
+      "thread 5",
+      thread_fn,
       30,
       pointer,
       STACK_SIZE,
@@ -80,23 +141,119 @@ void tx_application_define(void *first_unused_memory) {
    status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE, TX_NO_WAIT);
 
    status = tx_thread_create(
-      &thread3,
-      "thread 3",
-      thread_3,
+      &thread6,
+      "thread 6",
+      thread_fn,
       30,
       pointer,
-      STACK_SIZE, 
+      STACK_SIZE,
       3,
       3,
       TX_NO_TIME_SLICE,
       TX_AUTO_START
    );
+   
+   // C ---------------------------------------------------------------------------
+   
+   status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE,TX_NO_WAIT);
+
+   status = tx_thread_create(
+      &thread7,
+      "thread 7",
+      thread_fn,
+      30,
+      pointer,
+      STACK_SIZE,
+      1,
+      3,
+      50,
+      TX_AUTO_START
+   );
+
+   status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE, TX_NO_WAIT);
+   
+   status = tx_thread_create(
+      &thread8,
+      "thread 8",
+      thread_fn,
+      30,
+      pointer,
+      STACK_SIZE,
+      2,
+      3,
+      50,
+      TX_AUTO_START
+   );
+   
+   status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE, TX_NO_WAIT);
+
+   status = tx_thread_create(
+      &thread9,
+      "thread 9",
+      thread_fn,
+      30,
+      pointer,
+      STACK_SIZE,
+      3,
+      3,
+      50,
+      TX_AUTO_START
+   );
+   
+   // D ---------------------------------------------------------------------------
+   
+   status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE,TX_NO_WAIT);
+
+   status = tx_thread_create(
+      &thread10,
+      "thread 10",
+      thread_fn,
+      30,
+      pointer,
+      STACK_SIZE,
+      1,
+      3,
+      50,
+      TX_AUTO_START
+   );
+   
+   status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE, TX_NO_WAIT);
+   
+   status = tx_thread_create(
+      &thread11,
+      "thread 11",
+      thread_fn,
+      30,
+      pointer,
+      STACK_SIZE,
+      1,
+      1,
+      50,
+      TX_AUTO_START
+   );
+   
+   status = tx_byte_allocate(&byte_pool, (VOID**)&pointer, STACK_SIZE, TX_NO_WAIT);
+
+   status = tx_thread_create(
+      &thread12,
+      "thread 12",
+      thread_fn,
+      30,
+      pointer,
+      STACK_SIZE,
+      1,
+      1,
+      50,
+      TX_AUTO_START
+   );
 
    tx_block_release(pointer);
+   
+   // E => ?
 
 }
 
-void thread_1(ULONG thread_input) {
+void thread_fn(ULONG thread_input) {
    uint32_t ui32Loop = 0;
    uint32_t blink;
  
@@ -120,64 +277,6 @@ void thread_1(ULONG thread_input) {
       for(blink = 0; blink < 200000; blink++){}
 
       GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0x0);
-
-      ui32Loop++;
-   }
-
-   time_after = tx_time_get();
-   time_lapsed = time_after - time_before;
-}
-
-
-void thread_2(ULONG thread_input) {
-   uint32_t ui32Loop = 0;
-   uint32_t blink;
-
-   ULONG time_before;
-   ULONG time_after;
-   ULONG time_lapsed ;
-
-   GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0x0);
-   GPIOPinWrite(GPIO_PORTF_AHB_BASE, GPIO_PIN_4, 0x0);
-
-   time_before = tx_time_get();
-
-   while(ui32Loop < thread_input) {
-      for(blink = 0; blink < 200000; blink++) {}
-      
-      GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1);
-      
-      for(blink = 0; blink < 200000; blink++) {}
-      GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0x0);
-      ui32Loop++;
-   }
-
-   time_after = tx_time_get();
-   time_lapsed = time_after - time_before;
-}
-
-
-void thread_3(ULONG thread_input) {
-   uint32_t ui32Loop = 0;
-   uint32_t blink;
-   
-   ULONG time_before;
-   ULONG time_after;
-   ULONG time_lapsed;
-   
-   GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0x0);
-   GPIOPinWrite(GPIO_PORTF_AHB_BASE, GPIO_PIN_4, 0x0);
-   
-   time_before = tx_time_get();
-   
-   while(ui32Loop < thread_input) {
-      for(blink = 0; blink < 200000; blink++) {}
-      
-      GPIOPinWrite(GPIO_PORTF_AHB_BASE, GPIO_PIN_4, GPIO_PIN_4);
-      
-      for(blink = 0; blink < 200000; blink++) {}
-
-      GPIOPinWrite(GPIO_PORTF_AHB_BASE, GPIO_PIN_4, 0x0);
 
       ui32Loop++;
    }
